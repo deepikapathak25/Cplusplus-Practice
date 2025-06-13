05b0d62dac985d61690007a0b30b3f62711bf72f
deepikapathak25 <pathakdeepika43@gmail.com>
2025-06-13 20:03:10 +0530

    PairSum

--- Arrays/Vector/vector.cpp
+++ Arrays/Vector/vector.cpp
@@ -3,15 +3,30 @@
 using namespace std;
                       
 int main() {
-    vector<int> vec ;
-    cout<< "Size of vec= "<<vec.size()<<endl;
-    vec.push_back(25);
-    vec.push_back(35);
-    vec.push_back(45);
-    cout<< "After pushback Size of vec= "<<vec.size()<<endl;
-    vec.pop_back();
-    cout<< "After pushback Size of vec= "<<vec.size()<<endl;
+    // stl in vector
+
+    // vector<int> vec ;
+    // cout<< "Size of vec= "<<vec.size()<<endl;
+    // vec.push_back(25);
+    // vec.push_back(35);
+    // vec.push_back(45);
+    // cout<< "After pushback Size of vec= "<<vec.size()<<endl;
+    // vec.pop_back();
+    // cout<< "After pushback Size of vec= "<<vec.size()<<endl;
+    // cout<<vec.at(1)<<endl;
+
+
+
+    //Capacity & Size of vector
+    // vector<int> vec;
+    // vec.push_back(0);
+    // vec.push_back(1);
+    // vec.push_back(2);
+    // vec.push_back(3);
+    // vec.push_back(4);
+    // cout<<"Size of vector: "<<vec.size()<<endl; // Size=5
+    // cout<<"Capacity of vector: "<<vec.capacity()<<endl; // capacity  = 8
+
     
-    cout<<vec.at(1)<<endl;
     return 0;
 }
