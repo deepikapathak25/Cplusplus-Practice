43450aa0642d9de03ea13d2f19b6ffed30bb969e
deepikapathak25 <pathakdeepika43@gmail.com>
2025-06-20 10:55:55 +0530

    Add function to check if array is sorted in non-decreasing order

---
+++ Arrays/Check if array is Sorted/checkifArrayisSorted.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    bool arraySortedOrNot(vector<int>& nums) {
+        // code here
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] < nums[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
